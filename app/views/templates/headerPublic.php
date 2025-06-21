edf6bcddc75cc755e1804afcabdd3a6d4490b6b3
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-06-21 03:16:56 +0000

    Added basic css

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -6,8 +6,8 @@ if (isset($_SESSION['auth']) == 1) {
 
 <!DOCTYPE html>
 <html lang="en">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" href="/favicon.png">
+    <link rel="stylesheet" href="/css/style.css">
     <title>COSC 4806</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
